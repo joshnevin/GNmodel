c66e87f7c1796c243baf6d4240b852d5baa4066a
Josh Nevin <jn399@cam.ac.uk>
2020-04-22 16:00:34 +0100

    tweaked heteroscedastic kernel

--- heteroscedastickernel2.py
+++ heteroscedastickernel2.py
@@ -47,9 +47,9 @@ y = np.genfromtxt(open("SNRpath1.csv", "r"), delimiter=",", dtype =float)
 #wmean = np.sin(2.5*x)*np.sin(1.5*x)
 #wsd = 0.01 + 0.25*(1 - np.sin(2.5*x))**2
 #y = np.random.normal(wmean, wsd)
-#y = y.reshape(-1,1)
-#x = x.reshape(-1,1)
 
+y = y.reshape(-1,1)
+x = x.reshape(-1,1)
 plt.plot(x,y,'+')
 plt.show()
 
@@ -57,7 +57,7 @@ yraw = y
 ymean = np.mean(y)
 n = np.size(x)
 scaler = StandardScaler().fit(y)
-y = scaler.transform(y)
+#y = scaler.transform(y)
 
 numsig = 2
 
@@ -249,18 +249,11 @@ def hypopth(y, numrestarts, R):
             continue 
         k1s4[i] = step4res[0][0]
         k2s4[i] = step4res[0][1]
-    lmltest = [lml([k1s4[i],k2s4[i]],y,np.var(y)**0.5) for i in range(numrestarts)]
+    lmltest = [lmlh([k1s4[i],k2s4[i]],y,R) for i in range(numrestarts)]
     k1f = k1s4[np.argmin(lmltest)]
     k2f = k2s4[np.argmin(lmltest)]
         #lml(params,y,sig)
     return k1f, k2f
-
-rtest = exp(ystar)
-Rtest = rtest*np.identity(n)
-
-numrestarts = 10
-#test1,test2 = hypopt(y,10)
-test3,test4 = hypopth(y,10,Rtest)
 
 
 # %%
@@ -318,12 +311,36 @@ plt.show()
 varerr = varfmst**0.5 - sigma
 fiterr = fmst - ystar
 
+# %%
+
+#NLPD[i] = sum([(1/n)*(-np.log(norm.pdf(x[j], fmst4[j], varfmst4[j]**0.5))) for j in range(n) ])
+
+
+test3 = np.empty([n,1])
+for k in range(n):
+    test3[k] = -np.log(norm.pdf(x[k], fmst[k], varfmst[k]**0.5))
+test3 = sum(test3)*(1/n)
+
+
+test = norm.pdf(x[0], fmst[0], varfmst[0]**0.5)
+
+
+z = np.empty([n,1])
+s = 200
+for j in range(n):
+    np.random.seed()
+    normdraw = normal(fmst[j], varfmst[j]**0.5, s).reshape(s,1)
+    z[j] = np.log((1/s)*0.5*sum((y[j] - normdraw)**2))
+
+test2 = normdraw - test
+
+
 
 # %% Steps 2-4 in a loop - iterate until convergence 
 
 
 def hetloop(fmst,varfmst,numiters):
-    s = 5000
+    s = 500
     #k1is3, k2is3, k1is4,k2is4  =  np.random.uniform(1e-2,1e2,4)
     MSE = np.empty([numiters,1])
     NLPD = np.empty([numiters,1])
@@ -393,27 +410,28 @@ def hetloop(fmst,varfmst,numiters):
         
     return fmstf,varfmstf, lmloptf, MSE, NLPD 
 
-def hetloopSK(fmst,varfmst,numiters):
-    s = 150
+def hetloopSK(fmst,varfmst,numiters,numrestarts):
+    s = 200
     #k1is3, k2is3, k1is4,k2is4  =  np.random.uniform(1e-2,1e2,4)
     MSE = np.empty([numiters,1])
-    #NLPD = np.empty([numiters,1])
+    NLPD = np.empty([numiters,1])
     fmstf = np.empty([numiters,n])
     varfmstf = np.empty([numiters,n])
     lmloptf = np.empty([numiters,1])
+    rf = np.empty([numiters,n])
     i = 0
     while i < numiters:        
         # Step 2: estimate empirical noise levels z 
         #k1is4,k2is4  = np.random.uniform(1e-2,1e2,2)
-        k1is3, k2is3, k1is4,k2is4  =  np.random.uniform(1e-1,1e1,4)
+        k1is3, k2is3, k1is4,k2is4  =  np.random.uniform(1e-2,1e2,4)
         z = np.empty([n,1])
         for j in range(n):
             np.random.seed()
             normdraw = normal(fmst[j], varfmst[j]**0.5, s).reshape(s,1)
             z[j] = np.log((1/s)*0.5*sum((y[j] - normdraw)**2))
         #  Step 3: estimate GP2 on D' - (x,z)
-        kernel2 = C(k1is3, (1e-3, 1e3)) * RBF(k2is3, (1e-3, 1e3)) 
-        gpr2 = GaussianProcessRegressor(kernel=kernel2, n_restarts_optimizer = 10, normalize_y=False, alpha=np.var(z))
+        kernel2 = C(k1is3, (1e-2, 1e2)) * RBF(k2is3, (1e-2, 1e2)) 
+        gpr2 = GaussianProcessRegressor(kernel=kernel2, n_restarts_optimizer = numrestarts, normalize_y=False, alpha=np.var(z))
         gpr2.fit(x, z)
         ystar2, sigma2 = gpr2.predict(x, return_std=True )
         sigma2 = (sigma2**2 + 1)**0.5
@@ -421,42 +439,35 @@ def hetloopSK(fmst,varfmst,numiters):
     # Step 4: train heteroscedastic GP3 using predictive mean of G2 to predict log noise levels r
         r = exp(ystar2)
         R = r*np.identity(n)
-        #kis4 = np.ndarray((numh,), buffer=np.array([k1is4,k2is4]), dtype = float)
-        #s4res = minimize(lmlh,kis4,args=(y,R),method = 'L-BFGS-B',jac=lmlgh,bounds = ((1e-5,1e5),(1e-5,1e5)))
-        #step4res = []
-        #if s4res.success:
-        #    step4res.append(s4res.x)
-        #else:
-            #raise ValueError(s4res.message)
-        #    k1is4 = np.random.uniform(0.01,100)
-        #    k2is4 = np.random.uniform(0.01,100)
-        #    print("error - reinitialising hyperparameters")
-        #    continue 
-        #k1s4 = step4res[0][0]
-        #k2s4 = step4res[0][1]
-        k1s4, k2s4 = hypopth(y,5,R)
+        k1s4, k2s4 = hypopth(y,numrestarts,R)
         fmst4, varfmst4, lmlopt4 = GPRfith(x,k1s4,k2s4,R,R)
         # test for convergence 
         MSE[i] = (1/n)*sum(((y-fmst4)**2)/np.var(y))
         #NLPD[i] = sum([(1/n)*(-np.log(norm.pdf(x[j], fmst4[j], varfmst4[j]**0.5))) for j in range(n) ])
+        test3 = np.empty([n,1])
+        for k in range(n):
+            test3[k] = -np.log(norm.pdf(x[k], fmst[k], varfmst[k]**0.5))
+        print(norm.pdf(x[k], fmst[k], varfmst[k]**0.5))
+        NLPD[i] = sum(test3)*(1/n)
         print("MSE = " + str(MSE[i]))
-        #print("NLPD = " + str(NLPD[i]))
+        print("NLPD = " + str(NLPD[i]))
         print("finished iteration " + str(i+1))
         fmstf[i,:] = fmst4.reshape(n)
         varfmstf[i,:] = varfmst4.reshape(n)
         lmloptf[i] = lmlopt4
         fmst = fmst4
         varfmst = varfmst4
+        rf[i,:] = r.reshape(n)
         #k1is3 = k1s4
         #k2is3 = k2s4
         i = i + 1
         
-    return fmstf,varfmstf, lmloptf, MSE #  , NLPD 
+    return fmstf,varfmstf, lmloptf, MSE, rf, NLPD #  , NLPD 
 
 
 #kernel1 = C(1.0, (1e-3, 1e3)) * RBF(10, (1e-3, 1e3)) + W(1.0, (1e-5, 1e5))
 #gpr1 = GaussianProcessRegressor(kernel=kernel1, n_restarts_optimizer = 0, normalize_y=True)
-kernel1 = C(1.0, (1e-3, 1e3)) * RBF(1.0, (1e-3, 1e3)) 
+kernel1 = C(1.0, (1e-1, 1e1)) * RBF(1.0, (1e-2, 1e2)) 
 gpr1 = GaussianProcessRegressor(kernel=kernel1, n_restarts_optimizer = 10, normalize_y=False, alpha=np.var(y))
 gpr1.fit(x, y)
 ystar1, sigma1 = gpr1.predict(x, return_std=True )
@@ -464,10 +475,13 @@ var1 = (sigma1**2 + np.var(y))
 #sigma1 = np.reshape(sigma1,(np.size(sigma1), 1))
 
 # %%
-numiters = 15
+numiters = 20
+numrestarts = 10
 #fmstf,varfmstf, lmloptf, MSE, NLPD = hetloop(fmst,varfmst,numiters)
 #fmstf,varfmstf, lmloptf, MSE, NLPD = hetloop(fmst,varfmst-(sig1**2),numiters)
-fmstf,varfmstf, lmloptf, MSE = hetloopSK(ystar1,var1,numiters)
+fmstf,varfmstf, lmloptf, MSE, rf,NLPD = hetloopSK(ystar1,var1,numiters,numrestarts)
+
+
 # %% learning curve
 numiterations = np.linspace(1,numiters,numiters,dtype=int)
 plt.plot(numiterations,MSE)
@@ -477,11 +491,11 @@ plt.savefig('AMSE.png', dpi=200)
 plt.show()
 
 # =============================================================================
-# plt.plot(numiterations,NLPD)
-# plt.ylabel('NLPD')  
-# plt.xlabel('Iterations')
-# plt.savefig('ANLPD.png', dpi=200)
-# plt.show()
+plt.plot(numiterations,NLPD)
+plt.ylabel('NLPD')  
+plt.xlabel('Iterations')
+plt.savefig('ANLPD.png', dpi=200)
+plt.show()
 # =============================================================================
 
 plt.plot(numiterations, lmloptf)
@@ -497,7 +511,7 @@ ysam = [y[i:i + ns] for i in range(0, np.size(y), ns)]
 varsam = [np.var(ysam[i]) for i in range(np.size(ysam,0))]
 sigsam = [i**0.5 for i in varsam]
 xsig = np.linspace(x[0],x[n-1], np.size(ysam,0))
-yp = np.polyfit(xsig.reshape(np.size(ysam,0)),varsam,5)
+yp = np.polyfit(xsig.reshape(np.size(ysam,0)),varsam,2)
 pol = np.poly1d(yp)
 plt.plot(xsig,varsam,'o')
 plt.plot(x,pol(x))
@@ -509,7 +523,7 @@ plt.show()
 
 #   plotting
 ind = numiters - 1
-
+ind = 4
 fmst4 = fmstf[ind]
 varfmst4 = varfmstf[ind]
 lmlopt4 = lmloptf[ind]
@@ -528,7 +542,7 @@ plt.fill(np.concatenate([x, x[::-1]]),
                         (fmstms4)[::-1]]),
          alpha=0.3, fc='r', ec='None', label=labelfillj)
 plt.title("Shifted HGP")
-#plt.savefig('Aheteroscedasticgdata.pdf', dpi=200)
+plt.savefig('Aheteroscedasticgdata.pdf', dpi=200)
 plt.show()
 
 plt.plot(x,varfmst4,label='heteroscedastic GP')
@@ -537,7 +551,7 @@ plt.xlabel('$x$')
 plt.ylabel('$\sigma^2$')
 plt.legend()
 #plt.title("Shifted HGP sigma")
-#plt.savefig('Aheteroscedasticgsigma.pdf', dpi=200)
+#plt.savefig('Aheteroscedasticgvar.pdf', dpi=200)
 plt.show()
 
 plt.plot(x,sigs4,label='heteroscedastic GP')
@@ -546,37 +560,49 @@ plt.xlabel('$x$')
 plt.ylabel('$\sigma$')
 plt.legend()
 #plt.title("Shifted HGP sigma")
-#plt.savefig('Aheteroscedasticgsigma.pdf', dpi=200)
+plt.savefig('Aheteroscedasticgsigma.pdf', dpi=200)
 plt.show()
-
 
 # %%
 
-labelfillj2 = str(numsig) + '$\sigma$'
-# =============================================================================
-yi = scaler.inverse_transform(y)
-test = yi - yraw
-fmst4i = scaler.inverse_transform(fmst4)
-#varfmst4i = scaler.inverse_transform(varfmst4)
+plt.plot(x,rf[ind]**0.5,label='$\sigma = \sqrt{r(x)}$')
+plt.plot(x,sigs4,label='HGP sd')
+plt.plot(xsig, sigsam, 'o', label='sampled $\sigma$')
+plt.xlabel('$x$')
+#plt.ylabel('$\sigma$')
+plt.legend()
+plt.savefig('Anoisefunction.pdf', dpi=200)
+plt.show()
 
-fmstps4i = fmst4i + numsig*sigs4
-fmstms4i = fmst4i - numsig*sigs4
-#yana = 2*np.sin(2*np.pi*x)
-plt.plot(x,yi,'+')
-plt.plot(x,fmst4i, label = 'HGP')
-#plt.plot(x,yana, label = '$2sin(2{\pi}x)$')
+# %%
+
+# =============================================================================
+# labelfillj2 = str(numsig) + '$\sigma$'
+# # =============================================================================
+# yi = scaler.inverse_transform(y)
+# test = yi - yraw
+# fmst4i = scaler.inverse_transform(fmst4)
+# #varfmst4i = scaler.inverse_transform(varfmst4)
+# 
+# fmstps4i = fmst4i + numsig*sigs4
+# fmstms4i = fmst4i - numsig*sigs4
+# #yana = 2*np.sin(2*np.pi*x)
+# plt.plot(x,yi,'+')
+# plt.plot(x,fmst4i, label = 'HGP')
+# #plt.plot(x,yana, label = '$2sin(2{\pi}x)$')
+# # plt.fill(np.concatenate([x, x[::-1]]),
+# #          np.concatenate([fmstps4i,
+# #                         (fmstms4i)[::-1]]),
+# #          alpha=0.3, fc='r', ec='None', label=labelfillj)
 # plt.fill(np.concatenate([x, x[::-1]]),
 #          np.concatenate([fmstps4i,
 #                         (fmstms4i)[::-1]]),
-#          alpha=0.3, fc='r', ec='None', label=labelfillj)
-plt.fill(np.concatenate([x, x[::-1]]),
-         np.concatenate([fmstps4i,
-                        (fmstms4i)[::-1]]),
-         alpha=0.3, fc='r', ec='None', label=labelfillj2)
-plt.title("Raw HGP")
-plt.legend()
-plt.savefig('Aheteroscedasticgdatarescaled.pdf', dpi=200)
-plt.show()
+#          alpha=0.3, fc='r', ec='None', label=labelfillj2)
+# plt.title("Raw HGP")
+# plt.legend()
+# plt.savefig('Aheteroscedasticgdatarescaled.pdf', dpi=200)
+# plt.show()
+# =============================================================================
 # # 
 # plt.plot(x,sigs4i, label = 'data')
 # plt.title("Reshifted HGP sigma")
